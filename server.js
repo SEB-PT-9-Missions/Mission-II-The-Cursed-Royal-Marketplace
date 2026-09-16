592683509b138cb75cea3f34857aad77cd3bc957
amir <amiralsharabi2@gmail.com>
2026-09-16 21:02:36 +0300

    done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-
+// update
 const artifactRoutes = require('./routes/artifacts.routes')
 const reviewRoutes = require('./routes/reviews.routes')
 
